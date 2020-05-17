1642de367eb7912c33a1e1fdd3c7ffe57f445b54
Alex Omusoru <al3xo@uw.edu>
2020-05-17 11:48:41 -0700

    finalized state sums and added to main census

--- COVID_19_Datathon-master/Varun_Alex_Merged_Content/Alex Post Final Engineering.py
+++ COVID_19_Datathon-master/Varun_Alex_Merged_Content/Alex Post Final Engineering.py
@@ -22,8 +22,20 @@ def main():
     AND ss.dffd = f.DaysFromFirstDate
     '''
     df = pd.read_sql_query(qry, conn)
+
     df = df.drop(['Unnamed: 0','dffd','st'], axis=1)
     df.to_csv(r"C:\Users\Alex Omusoru\Documents\GitHub\Datathon2020\COVID_19_Datathon-master\Varun_Alex_Merged_Content\final_plus_state.csv", encoding="cp1252") 
+
+    qry2= '''
+    
+    SELECT DaysFromFirstDate as dffd, State as st, SUM(active) state_active, SUM(recovered) state_recovered, SUM(deceased) state_deceased
+    FROM final
+    GROUP BY DaysFromFirstDate, State
+    
+    '''
+    df2 = pd.read_sql_query(qry2, conn)
+
+    df2.to_csv(r"C:\Users\Alex Omusoru\Documents\GitHub\Datathon2020\COVID_19_Datathon-master\Varun_Alex_Merged_Content\states_act_rec_dec.csv", encoding="cp1252") 
 
 if __name__=="__main__":
     main()
