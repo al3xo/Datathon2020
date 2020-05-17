7c3fe6de5edbfa37ad197f34531a676f3b86a891
Alex Omusoru <al3xo@uw.edu>
2020-05-17 11:18:05 -0700

    updates to final + states

--- COVID_19_Datathon-master/Varun_Alex_Merged_Content/Alex Post Final Engineering.py
+++ COVID_19_Datathon-master/Varun_Alex_Merged_Content/Alex Post Final Engineering.py
@@ -12,17 +12,17 @@ def main():
 
     qry = '''
     WITH stateStats AS(
-        SELECT DaysFromFirstDate, State, SUM(active) state_active, SUM(recovered) state_recovered, SUM(deceased) state_deceased
+        SELECT DaysFromFirstDate as dffd, State as st, SUM(active) state_active, SUM(recovered) state_recovered, SUM(deceased) state_deceased
         FROM final
         GROUP BY DaysFromFirstDate, State
     )
 
     SELECT * FROM final f, stateStats ss
-    WHERE f.State = ss.State
-    AND ss.DaysFromFirstDate = f.DaysFromFirstDate
+    WHERE f.State = ss.st
+    AND ss.dffd = f.DaysFromFirstDate
     '''
     df = pd.read_sql_query(qry, conn)
-    df = df.drop(['Unnamed: 0'], axis=1)
+    df = df.drop(['Unnamed: 0','dffd','st'], axis=1)
     df.to_csv(r"C:\Users\Alex Omusoru\Documents\GitHub\Datathon2020\COVID_19_Datathon-master\Varun_Alex_Merged_Content\final_plus_state.csv", encoding="cp1252") 
 
 if __name__=="__main__":
