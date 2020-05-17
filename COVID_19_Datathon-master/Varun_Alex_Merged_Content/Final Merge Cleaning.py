f49bd861b5fe8ef9d247650fcc03d9fbca7f004c
Varun Venkatesh <varunven@uw.edu>
2020-05-16 17:38:51 -0700

    stupid as again

--- COVID_19_Datathon-master/Varun_Alex_Merged_Content/Final Merge Cleaning.py
+++ COVID_19_Datathon-master/Varun_Alex_Merged_Content/Final Merge Cleaning.py
@@ -1,5 +1,13 @@
+import numpy as np 
+import pandas as pd 
+from itertools import groupby
+from collections import OrderedDict
+import matplotlib.pyplot as plt 
+import seaborn as sns
+
 def main():
     final = pd.read_csv(r"C:\Users\dswhi\OneDrive\Documents\UW Class Work\Dubstech\Datathon 3\Datathon2020\COVID_19_Datathon-master\Varun_Alex_Merged_Content\final-merge.csv", encoding='unicode_escape')
+    print(final['Density'])
 
 
 if __name__=="__main__":
